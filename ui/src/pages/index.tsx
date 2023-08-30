721cf165748fc489cdcc0a5a9221cf2ece56ae92
Sebastian Crossa <sebastiancrossa@hotmail.com>
2023-08-30 12:35:32 -0600

    improvements to chat ui design

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { FormEvent, useEffect, useState } from "react";
+import { FormEvent, useEffect, useRef, useState } from "react";
 import io, { Socket } from "socket.io-client";
 
 const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "ws://127.0.0.1";
@@ -37,6 +37,8 @@ function useSocket() {
 
 export default function Home() {
   const socket = useSocket();
+  const messagesRef = useRef<HTMLDivElement | null>(null);
+
   const [newMessage, setNewMessage] = useState("");
   const [messages, setMessages] = useState<Array<Message>>([]);
 
@@ -46,7 +48,12 @@ export default function Home() {
     });
 
     socket?.on(NEW_MESSAGE_CHANNEL, (message: Message) => {
-      setMessages((prevState) => [message, ...prevState]);
+      setMessages((prevState) => [...prevState, message]);
+
+      // hack to scroll to the true bottom bc of weird handling
+      setTimeout(() => {
+        scrollToBottom();
+      }, 0);
     });
   }, [socket]);
 
@@ -60,10 +67,17 @@ export default function Home() {
     setNewMessage("");
   };
 
+  const scrollToBottom = () => {
+    if (messagesRef.current)
+      messagesRef.current.scrollTop = messagesRef.current.scrollHeight + 1000;
+  };
+
   return (
     <main className="flex flex-col w-full p-4 max-w-3xl m-auto">
       <div className="flex justify-between items-center border-b mb-5">
-        <h1 className="text-4xl font-bold mb-4">overly engineered chat</h1>
+        <h1 className="text-3xl font-bold mb-4 font-mono">
+          overly engineered chat
+        </h1>
 
         {socket?.active ? (
           <div className="flex items-center space-x-2">
@@ -78,15 +92,21 @@ export default function Home() {
         )}
       </div>
 
-      <div className="flex-1 overflow-y-scroll overflow-x-hidden space-y-2">
+      <div
+        className="flex-1 overflow-y-scroll overflow-x-hidden space-y-2 no-scrollbar"
+        ref={messagesRef}
+      >
         {messages.map((message) => (
           <div
             key={message.id}
             className="flex flex-col items-start space-y-1 mb-4 bg-gray-100 p-4 rounded-lg break-all"
           >
-            <span className="text-sm text-gray-400">
-              {new Date(message.createdAt).toLocaleString()}
-            </span>
+            <div className="flex items-center space-x-1">
+              <span className="text-sm text-gray-400">
+                {new Date(message.createdAt).toLocaleString()}
+              </span>
+              <span className="text-sm text-gray-400">(:{message.port})</span>
+            </div>
             <span className="text-lg">{message.message}</span>
           </div>
         ))}
